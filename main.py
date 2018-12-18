0cf16d61e009f338d97ccd0fa86ade7fffef91ad
Danya <daniel.artemiev@outlook.com>
2018-12-18 14:04:36 +0200

    Ready headers

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ header = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:63.0) Gecko/201
 print("Привет!!!\nЭто просто программа для тру пикабушников. Введи ник автора: \nПример: test")
 AuthorName=input()
 UrlPart1="https://pikabu.ru/@"+AuthorName #
-PageNumber=1
+PageNumber=2
 UrlPart2="?page="
 urlall=UrlPart1+UrlPart2+str(PageNumber)
 r = requests.get(urlall, headers = header) # передаем заголовок запроса, чтобы нас не распознали как бота
 b=bs4.BeautifulSoup(r.text, "html.parser")
-title = b.select('a.story__title-link.story__title-link_visited') # получаем названии истории
-title_text = title[0].getText()
-print(title_text)
+title = b.select('a.story__title-link') # получаем названии истории
+i=0
+title_texts = []
+for i in range(len(title)):
+    title_texts.append(title[i].getText())
+    print(title_texts[i])
