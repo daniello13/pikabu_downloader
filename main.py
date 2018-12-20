fde08ecc950e41c17f5ee90b9385d23b50bdf6b2
Danya <daniel.artemiev@outlook.com>
2018-12-20 04:47:26 +0200

    bugs DB

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import requests, bs4
+import requests, bs4, pymysql, re
 header = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:63.0) Gecko/20100101 Firefox/63.0'}
 print("Привет!!!\nЭто просто программа для тру пикабушников. Введи ник автора: \nПример: test")
 AuthorName=input()
@@ -18,12 +18,49 @@ while tmp!=1: # начало бесконечного цикла на переб
     b=bs4.BeautifulSoup(r.text, "html.parser")
     title = b.select('a.story__title-link') # получаем названии истории
     story = b.select('div.story__content-inner') #получаем текст истории
+    s = '\"'
     for i in range(len(title)):
-        title_texts.append(title[i].getText())
+        title_texts.append(str(title[i].getText()).replace("\"", re.escape(s)))
+       
         
     for tmp in range(len(story)):
-        body_texts.append(story[tmp].getText())
+        #.append(story[tmp].getText())
+        body_texts.append(str(story[tmp].getText()).replace("\"", re.escape(s)))
     if len(title)<10:
         break
-# начало записи в БД
+# начало работы с  БД
+tmp = 0
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',                             
+                             db='pikabu',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+try:
+    with connection.cursor() as cursor:
 
+        # SQL 
+        lists_sql=[]
+        sql = "CREATE TABLE `"+AuthorName+"`(id INT(10) NOT NULL AUTO_INCREMENT Primary key, header TEXT NOT NULL, history LONGTEXT);"
+        cursor.execute(sql)
+        lists_sql.append(sql)
+        #начинаем заносить данные в БД
+        for tmp in range(len(title_texts)):
+            sql=""
+            sql="INSERT INTO `"+AuthorName+"`(header, history) VALUES (\""+title_texts[tmp]+"\",\""+body_texts[tmp]+"\");"
+            lists_sql.append(sql)
+        finh = "".join(lists_sql)
+        cursor.execute(finh)
+         
+except Exception: 
+    #если возникло, таблица уже существует, просто обновляемся
+    for tmp in range(len(title_texts)):
+            sql=""
+            sql="INSERT INTO `"+AuthorName+"`(header, history) VALUES (\""+title_texts[tmp]+"\",\""+body_texts[tmp]+"\");"
+            lists_sql.append(sql)
+    finh = "".join(lists_sql)
+    print (finh)
+    cursor.execute(re.escape(finh))
+finally:
+    # Закрыть соединение (Close connection).      
+    connection.close()
